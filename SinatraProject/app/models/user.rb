6d79bee2adadf0e0ecb954d2657b5f5b5f763602
rilemoore <rilemoore@gmail.com>
2019-12-06 17:03:10 -0800

    Finished input validation

--- SinatraProject/app/models/user.rb
+++ SinatraProject/app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
     has_many :vehicles
     has_secure_password
+    validates :password, presence: true
+    validates :password, length: {minimum: 6}
+    validates :username, uniqueness: true
+    validates :username, presence: true
+
 end
   
--- SinatraProject/app/models/vehicle.rb
+++ SinatraProject/app/models/vehicle.rb
@@ -1,4 +1,8 @@
 class Vehicle < ActiveRecord::Base
     belongs_to :user
+    validates :make, presence: true
+    validates :model, presence: true
+    validates :money_invested, presence: true
+    validates :money_invested, numericality: {greater_than_or_equal_to: 0}
 end
   
